9da38f11b38e141ec84f3da24a296f065138c280
pedro novaes <pedromarcelino2006@hotmail.com>
2014-08-10 14:51:17 -0300

    mais atualizacoes

--- trabalho.c
+++ trabalho.c
@@ -6,39 +6,20 @@ int main(){
 	
 	//variaveis
 	FILE *arq_open;
-	int tamanho_tabela, numero_threads, paginas;
-	int i, j, cont;
-	int vetor[50];
+	int tamanho_tabela, numero_threads;
+	int cont = 1;
 
 	//leitura do arquivo texto
 	arq_open = fopen("teste.txt", "r");
 
-	cont = 1;
-	i = 0;
-
 	//extraindo todo o conteudo do arquivo
 	while(!feof(arq_open)){
-		if (cont == 1){ //armazenar numero de paginas
 			fscanf(arq_open, "%d", &tamanho_tabela);
-
-			//criacao do vetor de paginas
-			int vetor_paginas[tamanho_tabela];
-			cont++;
-			//i = 0;
-		}
-		if (cont == 2){ //armazenar numero de threads
 			fscanf(arq_open, "%d", &numero_threads);
-			cont++;
-			//i = 0;
-		}
 		
 	}
 	printf("%d\n", tamanho_tabela);
 	printf("%d\n", numero_threads);
-	for(j=0; j<=i; j++){
-		printf("%d ", vetor[j]);
-	}
-	printf("\n");
 
 	//fim da leitura do arquivo texto
 	fclose(arq_open);
