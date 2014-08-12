9100250a6504fa980594ae730469e40603e1d7bb
pedro novaes <pedromarcelino2006@hotmail.com>
2014-08-12 16:10:11 -0300

    finalizacao da leitura de arquivos

--- trabalho.c
+++ trabalho.c
@@ -1,7 +1,13 @@
+/*				Universidade Federal da Bahia
+		   Trabalho de Sistemas Operacionais - MATA58
+		   		   Professor Ricardo Rios
+		   	   Marcos Ricardo e Pedro Marcelino			*/
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 //Estrutura para identificar a thread e apontar para a tabela de memória
 typedef struct 
@@ -9,6 +15,7 @@ typedef struct
 	int id;
 	int *tabela;
 	int tamanho_da_tabela;
+	int paginas_acessadas[50];
 }thread_infos, *pthread_infos;
 
 int fim_fila = 0;
@@ -17,39 +24,62 @@ void * acessa_pagina(void *arg);
 
 int main(){
 	
-
-	FILE *arq_open;
 	int tamanho_tabela, numero_threads;
-	int cont = 1;
+	char string[50];
+	char *token;
 
-	arq_open = fopen("teste.txt", "r");
 
-	while(!feof(arq_open)){
-			fscanf(arq_open, "%d", &tamanho_tabela);
-			fscanf(arq_open, "%d", &numero_threads);
-	}
+
+	//extraindo as informações do arquivo
+	scanf("%d", &tamanho_tabela);
+	scanf("%d", &numero_threads);
+
+	printf("%d\n", tamanho_tabela);
+	printf("%d\n", numero_threads);
+
 
 	//Definindo tamanho da tabela e número de threads threads
 	int tabela[tamanho_tabela], i;
 	pthread_t threads[numero_threads];
 	thread_infos infos[numero_threads];
 
+	int j = 0;
 	//Criação das threads
 	for (i = 0; i < numero_threads; i++){
 		infos[i].id = i;
 		infos[i].tabela = tabela;
 		infos[i].tamanho_da_tabela = tamanho_tabela;
-		pthread_create(&(threads[i]), NULL, acessa_pagina, &infos[i]);
+		scanf("\n\r%[^\n]", string);
+		token = strtok(string," ");
+		while(token != NULL){
+			infos[i].paginas_acessadas[j] = atoi(token);
+			j++;
+			token = strtok(NULL," ");
+		}
+		infos[i].paginas_acessadas[j] = -1;
+		j = 0;
+	}
+		//scanf("\n\r%[^\n]", infos[i].paginas_acessadas);	
+		//pthread_create(&(threads[i]), NULL, acessa_pagina, &infos[i]);
+
+	for(i = 0; i < numero_threads; i++){
+		for(j = 0;; j++){
+			if(infos[i].paginas_acessadas[j] == -1)
+				break;
+			printf("%d ", infos[i].paginas_acessadas[j]);
+		}
+		printf("\n");
 	}
 
-	for (i = 0; i < numero_threads; i++)
-		pthread_join(threads[i],NULL);
-	
 
-	fclose(arq_open);
+	//for (i = 0; i < numero_threads; i++)
+	//	pthread_join(threads[i],NULL);
+	
 	return 0;
 }
 
+
+/*
 void * acessa_pagina(void *arg){
 	
 	FILE *arq_thread;
@@ -58,22 +88,22 @@ void * acessa_pagina(void *arg){
 	pthread_infos thread_atual = (pthread_infos) arg;
 	
 	sprintf(nome_arq, "thread-%d.txt",thread_atual->id);
-	arq_thread = fopen(nome_arq, "a");
+	arq_open = fopen(nome_arq, "a");
 
 	int acessos[] = {7,4,9,2,9,0,1,3,4}, i,j, tamanho_vetor;
 	int tamanho_tabela = thread_atual->tamanho_da_tabela;
 
 	tamanho_vetor = sizeof(acessos)/sizeof(int);
 	
-	//Loops para verifiação dos acessos e da tabela
-	for (j = 0; j <tamanho_vetor ; j++){
+	//Loops para verificação dos acessos e da tabela
+	for (int j = 0; j <tamanho_vetor ; j++){
 		for (i = 0; i < tamanho_tabela; i++){
 			if (thread_atual->tabela[i] != acessos[j] &&  i == (tamanho_tabela-1))  //Condição para encontrar ou não um acesso
 			{
 				
 				//Aqui vai dar pagefault e vai inserir uma pagina
 				
-				fprintf(arq_thread, "%d-fault\n", acessos[j]);
+				fprintf(nome_arq, "%d-fault\n", acessos[j]);
 
 				
 				//Iteração para manipulação da tabela com FCFS
@@ -107,10 +137,10 @@ void * acessa_pagina(void *arg){
 			else
 			{
 				//Página encontrada, escreve no arquivo
-				fprintf(arq_thread, "%d-mem\n", acessos[j]);
+				fprintf(nome_arq, "%d-mem\n", acessos[j]);
 			}
 		}
 	}
-	fclose(arq_thread);
+	fclose(nome_arq);
 //	printf("Thread numero %d e referencia %d \n", thread_atual->id, thread_atual->tabela[0]);
-}
+}*/
